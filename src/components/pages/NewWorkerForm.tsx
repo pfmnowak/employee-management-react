e0bfa397974d7d01ba53991b3c9d102d88e40c3a
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2023-01-11 23:35:30 +0100

    Fix display of the form on smaller screens

--- src/components/pages/NewWorkerForm.tsx
+++ src/components/pages/NewWorkerForm.tsx
@@ -34,101 +34,103 @@ const NewWorkerForm = (props: NewWorkerFormProps) => {
 	};
 
 	return (
-		<Card header='New worker form'>
-			<form className={classes.form} onSubmit={submitFormHandler}>
-				<div className={classes.form__field}>
-					<input
-						type='text'
-						className={classes.form__field__input}
-						ref={firstNameRef}
-						name='firstName'
-						id='firstName'
-						placeholder=' '
-						pattern='[A-Za-ząćęłńóśźżĄĆĘŁŃÓŚŹŻ]{2,30}'
-						required
-						autoFocus
-					/>
-					<label htmlFor='firstName' className={classes.form__field__label}>
-						First Name
-					</label>
-					<p className={classes.form__field__error}>Is your First Name correct?</p>
-				</div>
-				<div className={classes.form__field}>
-					<input
-						type='text'
-						className={classes.form__field__input}
-						ref={lastNameRef}
-						name='lastName'
-						id='lastName'
-						placeholder=' '
-						pattern='[A-Za-ząćęłńóśźżĄĆĘŁŃÓŚŹŻ]{2,30}'
-						required
-					/>
-					<label htmlFor='lastName' className={classes.form__field__label}>
-						Last Name
-					</label>
-					<p className={classes.form__field__error}>Is your Last Name correct?</p>
-				</div>
-				<div className={classes.form__field}>
-					<select
-						className={classes.form__field__input}
-						ref={departmentRef}
-						name='department'
-						id='department'
-						required
-					>
-						{props.departments
-							.sort((a, b) => {
-								return ('' + a).localeCompare(b);
-							})
-							.map(option => (
-								<option key={option} value={option} label={option} />
-							))}
-					</select>
-					<label htmlFor='department' className={classes.form__field__label}>
-						Department
-					</label>
-				</div>
-				<div className={classes['form__currency-box']}>
+		<div className={classes.form__container}>
+			<Card header='New worker form'>
+				<form className={classes.form} onSubmit={submitFormHandler}>
 					<div className={classes.form__field}>
 						<input
-							type='number'
+							type='text'
 							className={classes.form__field__input}
-							ref={salaryRef}
-							name='salary'
-							id='salary'
+							ref={firstNameRef}
+							name='firstName'
+							id='firstName'
 							placeholder=' '
-							min={1000}
-							max={200000}
+							pattern='[A-Za-ząćęłńóśźżĄĆĘŁŃÓŚŹŻ]{2,30}'
+							required
+							autoFocus
+						/>
+						<label htmlFor='firstName' className={classes.form__field__label}>
+							First Name
+						</label>
+						<p className={classes.form__field__error}>Is your First Name correct?</p>
+					</div>
+					<div className={classes.form__field}>
+						<input
+							type='text'
+							className={classes.form__field__input}
+							ref={lastNameRef}
+							name='lastName'
+							id='lastName'
+							placeholder=' '
+							pattern='[A-Za-ząćęłńóśźżĄĆĘŁŃÓŚŹŻ]{2,30}'
 							required
 						/>
-						<label htmlFor='salary' className={classes.form__field__label}>
-							Salary
+						<label htmlFor='lastName' className={classes.form__field__label}>
+							Last Name
 						</label>
-						<p className={classes.form__field__error}>Is your Salary correct? (1k - 200k)</p>
+						<p className={classes.form__field__error}>Is your Last Name correct?</p>
 					</div>
-					<select
-						className={`${classes.form__field__input} ${classes['form__field__input--short']}`}
-						ref={currencyRef}
-						name='currency'
-						id='currency'
-						required
-					>
-						{props.currencies
-							.sort((a, b) => {
-								return ('' + a).localeCompare(b);
-							})
-							.map(option => (
-								<option key={option} value={option} label={option} />
-							))}
-					</select>
-				</div>
-				<br />
-				<button className={classes.form__button} type='submit' id='submit-button'>
-					Add new user
-				</button>
-			</form>
-		</Card>
+					<div className={classes.form__field}>
+						<select
+							className={classes.form__field__input}
+							ref={departmentRef}
+							name='department'
+							id='department'
+							required
+						>
+							{props.departments
+								.sort((a, b) => {
+									return ('' + a).localeCompare(b);
+								})
+								.map(option => (
+									<option key={option} value={option} label={option} />
+								))}
+						</select>
+						<label htmlFor='department' className={classes.form__field__label}>
+							Department
+						</label>
+					</div>
+					<div className={classes['form__currency-box']}>
+						<div className={classes.form__field}>
+							<input
+								type='number'
+								className={classes.form__field__input}
+								ref={salaryRef}
+								name='salary'
+								id='salary'
+								placeholder=' '
+								min={1000}
+								max={200000}
+								required
+							/>
+							<label htmlFor='salary' className={classes.form__field__label}>
+								Salary
+							</label>
+							<p className={classes.form__field__error}>Is your Salary correct? (1k - 200k)</p>
+						</div>
+						<select
+							className={`${classes.form__field__input} ${classes['form__field__input--short']}`}
+							ref={currencyRef}
+							name='currency'
+							id='currency'
+							required
+						>
+							{props.currencies
+								.sort((a, b) => {
+									return ('' + a).localeCompare(b);
+								})
+								.map(option => (
+									<option key={option} value={option} label={option} />
+								))}
+						</select>
+					</div>
+					<br />
+					<button className={classes.form__button} type='submit' id='submit-button'>
+						Add new user
+					</button>
+				</form>
+			</Card>
+		</div>
 	);
 };
 
